0b5bd7c41765dbca49737f1f6da9ee0e5a49ede7
Mutsumi Imamura <imamura@tejimaya.com>
2013-11-20 16:55:59 +0900

    Revert "opTimelinePlugin1.2.1の追加"
    This reverts commit 99a10dd5bb9f8c08d2f328263c4a09a30ea4a4d1.

--- plugins/opTimelinePlugin/web/winter/js/license.js
+++
@@ -1,6 +0,0 @@
-/**
- * license
- */
-$(document).ready(function(){
-    $('#license').html('Copyright by <a target"_new" href="http://earthincolors.wordpress.com" rel="nofollow">Moyan Brenn</a>');
-});
